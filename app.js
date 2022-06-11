3094433ad4de8bbcf6c59f13851bfff7cb22a163
Gustavo Aquino <inggaquinoo@gmail.com>
2022-06-11 00:29:35 -0500

    App.js change password and .env

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.use("/api/v1", User);
 
-app.get("/",(req,res)=>{
-    res.send("Hi New World, Server is Working");
-})
+app.get("/", (req, res) => {
+    res.send("Server is working");
+  });
 
